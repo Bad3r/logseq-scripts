425b54a193082e48bdcb49d4955ea2bf1a52948b
Bad3r <bad3r@protonmail.com>
2023-05-07 23:19:15 -0400

    Create LogseqHTTPServerReposition.js

---
+++ LogseqHTTPServerReposition.js
@@ -0,0 +1,8 @@
+// Reposition HTTP server button to the right
+const rightToolbar = document.querySelector('#head .r');
+if (rightToolbar) {
+  const httpServerButton = document.querySelector('.cp__server-indicator');
+  if (httpServerButton) {
+    rightToolbar.insertAdjacentElement('afterbegin', httpServerButton);
+  }
+}
